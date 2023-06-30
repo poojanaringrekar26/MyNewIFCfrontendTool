ce4958e2a88ee6d88ada483236d91fddb8b71176
poojanaringrekar <pooja.naringrekar@digitaltwin.technology>
2023-06-30 15:50:21 +0200

    adding ifc file into the project

--- app.js
+++ app.js
@@ -61,3 +61,41 @@ export async function queryComunica() {
 
 }
 window.queryComunica = queryComunica;
+
+  // Plot in results box
+  //clear result box
+  document.getElementById("results-box-content").innerHTML = "";
+  //start table component
+  let tableContent = "<table>" 
+
+  tableContent +="<tr>"
+  const headers = bindings[0].entries._root.entries
+  for (var y = 0; y<headers.length; y++) {
+    tableContent += "<th>"+headers[y][0]+"</th>"
+  }
+  tableContent +="</tr>"
+
+  for (var i = 0; i<bindings.length; i++) {
+    const binding1 = bindings[i]
+    const results = binding1.entries._root.entries
+
+    //create table rows
+    tableContent += "<tr>"
+    for (var x = 0; x<results.length; x++) {
+      if (results[x][1].termType === "Literal") {
+        console.log(results[x][0] +": "+ results[x][1].value)
+        
+        tableContent += "<td>"+results[x][1].value+"</td>"
+      }
+      if (results[x][1].termType !== "Literal") {
+        console.log(results[x][0] +": "+ results[x][1].value.split("#", 2)[1])
+        tableContent += "<td><a target='_blank' href='"+results[x][1].value+"'>"+results[x][1].value.split("#", 2)[1]+"</a></td>"
+      }
+    }
+    tableContent += "</tr>"
+  } 
+  //end
+  tableContent += "</table>" 
+
+  //add table to results box
+  document.getElementById("results-box-content").innerHTML = tableContentno
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = {
 entry: './app.js',
+mode: 'development',
 output: {
 filename: 'bundle.js',
 path: path.resolve(__dirname, './'),
